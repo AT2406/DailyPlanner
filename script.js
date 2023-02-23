9a6fdbca9064b7e5780299f96654c9d39c35990a
AT2406 <118459009+AT2406@users.noreply.github.com>
2023-02-23 16:15:10 +0000

    fixed task adding and improved visuals, fix radio + style#

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const task_list = [];
 let nameOfTask;
 let descOfTask;
 n = 0;
 taskItem = { name: nameOfTask, description: descOfTask };
 
-/* set first div in html to task 1, 
-array object holding title and desc strings
-pull vlues from dom and make object, then push object to array */
+const task_list = [];
 
+/*Function used to store task info */
 function recordItemInfo() {
   taskItem = { nameOfTask, descOfTask };
   task_list.push(taskItem);
@@ -20,22 +18,31 @@ function readInput() {
     alert('*Enter values');
   } else {
     recordItemInfo();
-    document.getElementById('test').innerHTML = JSON.stringify(task_list);
+    // document.getElementById('test').innerHTML = JSON.stringify(task_list);
+    addNewTaskBox();
   }
-  addNewTaskBox();
+  
 }
 
 function addNewTaskBox() {
+  /*Select all elements with class tasks and returns array-like object called a nodelist*/
   const divToDuplicate = document.getElementsByClassName('tasks');
+  /*Selects the first item in the nodelist(the initial task box) and duplicates it with all contents/attrs*/
   const addedTask = divToDuplicate[0].cloneNode(true);
+  /*Create var to references new task button*/
   const button = document.getElementById('addNewTask');
-
+  /*Create var for radio button*/
+  const complete = document.getElementById('progress');
+  /* */
   addedTask.getElementsByClassName('taskName')[0].value = '';
+  /* */
   addedTask.getElementsByClassName('taskDesc')[0].value = '';
-
+  /* */
   addedTask.id = 'task' + ++n;
   addedTask.getElementsByClassName('taskName')[0].id = 'task' + n + 'Name';
   addedTask.getElementsByClassName('taskDesc')[0].id = 'task' + n + 'Desc';
 
+  /*Select the radio button and set it to unchecked in duplicate*/
+  // complete.checked = false;
   button.parentNode.insertBefore(addedTask, button);
 }
